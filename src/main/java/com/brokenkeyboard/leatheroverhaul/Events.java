b653a25bb53f22c5a1b48d1325199f0082963632
CrackedScreen <jonho2098@gmail.com>
2023-05-14 18:20:57 -0500

    Add item predicates for resource packs

--- src/main/java/com/brokenkeyboard/leatheroverhaul/Events.java
+++ src/main/java/com/brokenkeyboard/leatheroverhaul/Events.java
@@ -2,19 +2,23 @@ package com.brokenkeyboard.leatheroverhaul;
 
 import com.brokenkeyboard.leatheroverhaul.datagen.conditions.HideBundleCondition;
 import com.brokenkeyboard.leatheroverhaul.datagen.conditions.LeatherBundleCondition;
+import com.brokenkeyboard.leatheroverhaul.item.LeatherArmor;
+import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.DyeableLeatherItem;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterColorHandlersEvent;
 import net.minecraftforge.common.crafting.CraftingHelper;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegisterEvent;
 
 public class Events {
 
     @Mod.EventBusSubscriber(modid = LeatherOverhaul.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class ModEvents {
+    public static class RegistryEvents {
 
         @SubscribeEvent
         public static void registerSerializers(RegisterEvent event) {
@@ -22,10 +26,31 @@ public class Events {
             CraftingHelper.register(HideBundleCondition.SERIALIZER);
             CraftingHelper.register(LeatherBundleCondition.SERIALIZER);
         }
+
+        @SubscribeEvent
+        public static void setup(final FMLClientSetupEvent event) {
+            event.enqueueWork(() -> {
+                ItemProperties.register(LeatherOverhaul.LEATHER_HELMET.get(),
+                        new ResourceLocation(LeatherOverhaul.MOD_ID, "bonus_armor"),
+                        (stack, world, living, id) -> living != null && LeatherArmor.getBonusArmor(stack) > 0 ? 1.0F : 0.0F);
+
+                ItemProperties.register(LeatherOverhaul.LEATHER_CHESTPLATE.get(),
+                        new ResourceLocation(LeatherOverhaul.MOD_ID, "bonus_armor"),
+                        (stack, world, living, id) -> living != null && LeatherArmor.getBonusArmor(stack) > 0 ? 1.0F : 0.0F);
+
+                ItemProperties.register(LeatherOverhaul.LEATHER_LEGGINGS.get(),
+                        new ResourceLocation(LeatherOverhaul.MOD_ID, "bonus_armor"),
+                        (stack, world, living, id) -> living != null && LeatherArmor.getBonusArmor(stack) > 0 ? 1.0F : 0.0F);
+
+                ItemProperties.register(LeatherOverhaul.LEATHER_BOOTS.get(),
+                        new ResourceLocation(LeatherOverhaul.MOD_ID, "bonus_armor"),
+                        (stack, world, living, id) -> living != null && LeatherArmor.getBonusArmor(stack) > 0 ? 1.0F : 0.0F);
+            });
+        }
     }
 
     @Mod.EventBusSubscriber(modid = LeatherOverhaul.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ModClientEvents {
+    public static class ClientEvents {
 
         @SubscribeEvent
         public static void colorItems(RegisterColorHandlersEvent.Item event) {
